914efd49cd7ccdf6a6a43151869ad6e548a52d29
vivek kumar <codervivek5@gmail.com>
2025-02-15 18:08:06 +0530

    added logout funtionality'

---
+++ Pandas Practice/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("ruchikakumbhar/calories-burnt-prediction")
+
+print("Path to dataset files:", path)
---
+++ Pandas Practice/main.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+# path = kagglehub.dataset_download("ruchikakumbhar/calories-burnt-prediction")
+
+# print("Path to dataset files:", path)
+
